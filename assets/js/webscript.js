d670e97a263404e4bd71f3a7ef1e0c2832503b39
Rehaman786 <rehaman.syed1990@gmail.com>
2021-02-25 15:41:42 +0530

    contactpage, tender, sample

--- assets/js/webscript.js
+++ assets/js/webscript.js
@@ -1,5 +1,115 @@
 // pageloade popup
 
 $(document).ready(function() {
-    $("#pagepopup").modal('show');
+    // $("#pagepopup").modal('show');
 });
+
+// Tenders Vertical scroll 
+
+
+$('#demo').marquee({
+
+
+    enable: true,
+
+    // scroll direction
+    // 'vertical' or 'horizontal'
+    direction: 'vertical',
+
+    // children items
+    itemSelecter: 'li',
+
+    // animation delay
+    delay: 3000,
+
+    // animation speed
+    speed: 1,
+
+    // animation timing
+    timing: 1,
+
+    // mouse hover to stop the scroller
+    mouse: true
+
+});
+
+
+// Chart js
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Task', 'Hours per Day'],
+        ['Total Occupied', 11],
+        ['Vacant', 2],
+    ]);
+
+
+    var options = {
+        title: 'Storage Status',
+        'backgroundColor': {
+            'fill': '#FCFCFC',
+            'opacity': 100
+        }
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
+}
+
+
+
+// Bottom Up
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+// Font increase or Decerase
+
+
+var $affectedElements = $("p, h1, a"); // Can be extended, ex. $("div, p, span.someClass")
+
+
+$affectedElements.each(function() {
+    var $this = $(this);
+    $this.data("orig-size", $this.css("font-size"));
+});
+
+$("#btn-increase").click(function() {
+    changeFontSize(1);
+})
+
+$("#btn-decrease").click(function() {
+    changeFontSize(-1);
+})
+
+$("#btn-orig").click(function() {
+    $affectedElements.each(function() {
+        var $this = $(this);
+        $this.css("font-size", $this.data("orig-size"));
+    });
+})
+
+function changeFontSize(direction) {
+    $affectedElements.each(function() {
+        var $this = $(this);
+        $this.css("font-size", parseInt($this.css("font-size")) + direction);
+    });
+}
